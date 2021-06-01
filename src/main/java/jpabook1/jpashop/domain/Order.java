7f775e71cf777e9302d7a4bd118ff9f2c07bc241
Valium-K <lschan96@gmail.com>
2021-06-01 17:03:18 +0900

    연관 관계 개선
    외래키 값 -> 객체 매핑

--- src/main/java/jpabook1/jpashop/domain/Order.java
+++ src/main/java/jpabook1/jpashop/domain/Order.java
@@ -2,6 +2,8 @@ package jpabook1.jpashop.domain;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "ORDERS") // DB의 order 가 예약어라 orders로 관례상 적음
@@ -9,15 +11,49 @@ public class Order {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    @Column(name = "ITEM_ID")
+    @Column(name = "ORDER_ID")
     private Long id;
 
-    @Column(name = "MEMBER_ID")
-    private Long memberId;
+    /**
+     *     더이상 외래키 값으로 직접 연관관계를 맺지 않는다.
+     *     객체 자체를 연관관계로 맺는다.
+     *     @Column(name = "MEMBER_ID")
+     *     private Long memberId;
+     */
 
+    @ManyToOne
+    @JoinColumn(name = "MEMBER_ID")
+    private Member member;
+
+    @OneToMany(mappedBy = "order")
+    private List<OrderItem> orderItemList = new ArrayList<>();
     private LocalDateTime orderDate;
 
     @Enumerated(EnumType.STRING)
     private OrderStatus status;
 
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public Member getMember() {
+        return member;
+    }
+    public void setMember(Member member) {
+        this.member = member;
+    }
+    public LocalDateTime getOrderDate() {
+        return orderDate;
+    }
+    public void setOrderDate(LocalDateTime orderDate) {
+        this.orderDate = orderDate;
+    }
+    public OrderStatus getStatus() {
+        return status;
+    }
+    public void setStatus(OrderStatus status) {
+        this.status = status;
+    }
 }
--- src/main/java/jpabook1/jpashop/domain/OrderItem.java
+++ src/main/java/jpabook1/jpashop/domain/OrderItem.java
@@ -11,11 +11,25 @@ public class OrderItem {
     @Column(name = "ORDER_ITEM_ID")
     private Long id;
 
-    @Column(name = "ORDER_ID")
-    private Long orderId;
+    /**
+     *     더이상 외래키 값으로 직접 연관관계를 맺지 않는다.
+     *     객체 자체를 연관관계로 맺는다.
+     *     @Column(name = "ORDER_ID")
+     *     private Long orderId;
+     *
+     *     @Column(name = "ITEM_ID")
+     *     private Long itemId;
+     */
 
-    @Column(name = "ITEM_ID")
-    private Long itemId;
+    @ManyToOne
+    @JoinColumn(name = "ORDER_ID")
+    private Order order;
+
+    // 단방향으로 설계했다
+    // 상품 자체로
+    @ManyToOne
+    @JoinColumn(name = "ITEM_ID")
+    private Item item;
 
     private LocalDateTime orderDate;
 
@@ -28,18 +42,7 @@ public class OrderItem {
     public void setId(Long id) {
         this.id = id;
     }
-    public Long getOrderId() {
-        return orderId;
-    }
-    public void setOrderId(Long orderId) {
-        this.orderId = orderId;
-    }
-    public Long getItemId() {
-        return itemId;
-    }
-    public void setItemId(Long itemId) {
-        this.itemId = itemId;
-    }
+
     public LocalDateTime getOrderDate() {
         return orderDate;
     }
